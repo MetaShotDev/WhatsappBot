3b37f09b4ad7ea55df238dca15386240d9f4be20
unniznd <aanandunnis@gmail.com>
2023-08-08 15:35:06 +0530

    production server ready

--- whatsappbot/prod_settings.py
+++ whatsappbot/prod_settings.py
@@ -1,15 +1,30 @@
 import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
+CORS_ALLOW_HEADERS = ['*']
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+
 
 DATABASES = {
     'default': {
-        'ENGINE': '',
-        'NAME': '',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST':os.getenv("DB_HOST"),
+        'PORT':os.getenv("DB_PORT"),
     }
 }
