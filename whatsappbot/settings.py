2e4c576b86f5a97c94695ad63e2c005feb24287d
unniznd <aanandunnis@gmail.com>
2023-09-24 10:39:30 +0530

    early beta for doc feature

--- whatsappbot/settings.py
+++ whatsappbot/settings.py
@@ -130,6 +130,10 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR/"static/"
 
+MEDIA_URL = '/temp/'
+MEDIA_ROOT = BASE_DIR/"temp/"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- whatsappbot/urls.py
+++ whatsappbot/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from hook.views import send_whatsapp_message
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('send-whatsapp-message/', send_whatsapp_message)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
