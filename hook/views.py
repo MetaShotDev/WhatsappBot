a72192a455aee11938af47c2fb6036da596f3cfb
unniznd <aanandunnis@gmail.com>
2023-11-10 15:48:46 +0530

    prompt improved

--- hook/views.py
+++ hook/views.py
@@ -343,7 +343,7 @@ def handle_incoming_message(message_data):
             messenger.send_message(text_body, sender_phone_number)
             return HttpResponse({'status': 'success'})
         
-        text_body += " Make response to 100words"
+        text_body += " Make response to 100words if needed otherwise keep it as it is"
 
         completion = openai.ChatCompletion.create(
             model="gpt-4",
